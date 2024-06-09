83f65904864a9ab69cdd94548bf8a5233473b047
Aoki <aokitakeshi57@gmail.com>
2024-06-09 20:04:59 +0700

    Portofolio

---
+++ js/aos-init.js
@@ -0,0 +1,5 @@
+AOS.init({
+  duration: 700,
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+});
